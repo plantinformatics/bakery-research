c63477e3a7a82e4a5d314513da3db125d8e1a8cb
Don-Isdale <Don.Isdale@gmail.com>
2026-09-21 16:50:20 +1000

    Add to prompt : ... describe Genolink as the standard way to look up AGG accessions by name ...
    Query.py : Append to global_instruction_and_information :
    When describing how to use Pretzel, always describe Genolink as the standard way to look up AGG accessions by name, to find the genotype ID required for example when selecting accessions in Pretzel.

--- GraphRAG/Query.py
+++ GraphRAG/Query.py
@@ -188,6 +188,8 @@ When referencing Pretzel datasets, only refer to datasets exactly as they are in
 
 When reporting accessions that carry specific genes, do not refer to accessions or varieties into which genes were transferred or introgressed. For example, if Lr46 was transferred into Avocet, do not list Avocet as a carrier of the gene unless the new accessions carrying the gene has a distinct name to differentiate it from the original accession that does not carry the gene.
 
+When describing how to use Pretzel, always describe Genolink as the standard way to look up AGG accessions by name, to find the genotype ID required for example when selecting accessions in Pretzel.
+
 """
 
 
